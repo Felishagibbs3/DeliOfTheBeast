48c054f1d01bcb80aed4c1f099a817dfec47408c
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-27 10:19:50 -0500

    add chip constructor, getters and add price, type

--- DeliOfTheBeast/src/main/java/com/pluralsight/models/Chips.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/models/Chips.java
@@ -1,6 +1,17 @@
 package com.pluralsight.models;
 
 public class Chips {
+    private String type;
+    private double price = 1.50;
+
+    public Chips(String type) {
+        this.type = type;
+    }
+
+    public String getType(){
+        return type;
+    }
     public double getPrice() {
+        return price;
     }
 }
