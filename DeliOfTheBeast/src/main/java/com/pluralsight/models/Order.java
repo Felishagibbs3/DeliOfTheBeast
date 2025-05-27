d6550bc719bb36282d600223b8e2b46aa065e6f9
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-27 09:01:35 -0500

    add sandwich class getter/setter/constructor/ total price

--- DeliOfTheBeast/src/main/java/com/pluralsight/models/Order.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,59 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    private List<Sandwich> sandwiches = new ArrayList<>();
+    private Drink drink;
+    private Chips chips;
+
+    public Order(List<Sandwich> sandwiches, Drink drink, Chips chips) {
+        this.sandwiches = sandwiches;
+        this.drink = drink;
+        this.chips = chips;
+    }
+
+    public List<Sandwich> getSandwiches() {
+        return sandwiches;
+    }
+
+    public void setSandwiches(List<Sandwich> sandwiches) {
+        this.sandwiches = sandwiches;
+    }
+
+    public Drink getDrink() {
+        return drink;
+    }
+
+    public void setDrink(Drink drink) {
+        this.drink = drink;
+    }
+
+    public Chips getChips() {
+        return chips;
+    }
+
+    public void setChips(Chips chips) {
+        this.chips = chips;
+    }
+
+    public void addSandwich (Sandwich sandwich) {
+        sandwiches.add(sandwich);
+    }
+
+    public double totalPrice(){
+        double total = 0.0;
+
+        for (Sandwich s : sandwiches){
+            total += s.calculatePrice();
+        }
+        if (drink != null) {
+            total += drink.getPrice();
+        }
+        if (chips != null) {
+            total += chips.getPrice();
+        }
+        return total;
+    }
 }
--- DeliOfTheBeast/src/main/java/com/pluralsight/models/Sandwich.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/models/Sandwich.java
@@ -1,4 +1,27 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Sandwich {
+    private String bread;
+    private String size;
+    private boolean toasted;
+    private List<String> meat = new ArrayList<>();
+    private List<String> cheese = new ArrayList<>();
+    private List<String> toppings = new ArrayList<>();
+    private List<String> sauce = new ArrayList<>();
+    private int extraMeat = 0;
+    private int extraCheese = 0;
+
+    public Sandwich(String bread, String size) {
+        this.bread = bread;
+        this.size = size;
+    }
+    public void addMeat(String meats) {
+        meat.add(meats);
+    }
+
+    public double calculatePrice() {
+    }
 }
