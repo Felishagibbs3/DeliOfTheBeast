46bd34d79c7501e0064e224766ff01907c04b44f
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-27 10:14:11 -0500

    add drink getter and switch statement to get price by size

--- DeliOfTheBeast/src/main/java/com/pluralsight/models/Drink.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/models/Drink.java
@@ -1,6 +1,41 @@
 package com.pluralsight.models;
 
 public class Drink {
-    public double getPrice() {
+
+    private String size;
+    private String flavor;
+    private double price;
+
+    public Drink(String size, String flavor, double price) {
+        this.size = size;
+        this.flavor = flavor;
+        this.price = price;
+
+        switch (size) {
+            case "Small":
+                this.price = 2.00;
+                break;
+            case "Medium":
+                this.price = 2.50;
+                break;
+            case "Large":
+                this.price = 3.00;
+                break;
+            default:
+                this.price = 0.0;
+        }
     }
+
+    public String getSize() {
+        return size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
 }
