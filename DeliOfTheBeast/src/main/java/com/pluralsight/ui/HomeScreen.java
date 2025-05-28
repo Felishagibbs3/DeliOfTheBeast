623545396298c82f779c31308be3193cb9d70dbb
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-28 11:17:23 -0500

    home screen setup

--- DeliOfTheBeast/src/main/java/com/pluralsight/ui/HomeScreen.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/ui/HomeScreen.java
@@ -1,4 +1,23 @@
 package com.pluralsight.ui;
 
+import java.util.Scanner;
+
 public class HomeScreen {
+    public void display() {
+        boolean running = true;
+        Scanner scanner = new Scanner(System.in);
+
+        while (running) {
+            System.out.println("Welcome to Deli of the Beast!");
+            System.out.println("Ready to Feast? Let's get started!");
+            System.out.println("1. New Order");
+            System.out.println("0. Exit");
+            String choice = scanner.nextLine();
+
+            if (choice.equals("1")) new OrderScreen().startOrder();
+            else if (choice.equals("0"))
+                break;
+
+        }
+    }
 }
