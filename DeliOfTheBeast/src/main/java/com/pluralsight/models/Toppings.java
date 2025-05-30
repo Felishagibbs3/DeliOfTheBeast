2fb576e7c5e2fb4d1daab8d8cd5bdfab018d5094
Felisha Gibbs <Felishagibbs3@gmail.com>
2025-05-30 11:42:32 -0500

    update toppings class

--- DeliOfTheBeast/src/main/java/com/pluralsight/models/Toppings.java
+++ DeliOfTheBeast/src/main/java/com/pluralsight/models/Toppings.java
@@ -1,4 +1,31 @@
 package com.pluralsight.models;
 
 public class Toppings {
+    private String name;
+
+    public Toppings(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public class addToppings{
+        public static Toppings createToppings(String name) {
+            switch (name.toLowerCase()) {
+                case "lettuce":
+                case "tomatoes":
+                case "jalapenos":
+                case "cucumbers":
+                case "guac":
+                case "peppers":
+                case "onions":
+                case "mushrooms":
+                case "pickles":
+
+            }
+
+            return new Toppings(name);
+        }
+    }
 }
